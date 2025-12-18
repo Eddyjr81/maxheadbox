a41eb931bcc5eb51cb419eba7bef129bba7b9183
syxanash <si.marzulli@gmail.com>
2025-12-18 12:30:23 +0100

    removed useless parameter from example tool

--- src/tools/calcSum.js
+++ src/tools/calcSum.js
@@ -2,7 +2,7 @@ const name = 'calc_sum';
 const params = "a, b";
 const description = 'calculate the sum of two numbers e.g. calc_sum(1, 2)';
 
-const execution = async (_backend_url, params) => {
+const execution = async (params) => {
   return params.split(",").reduce((acc, num) => acc + Number(num), 0);
 };
 
